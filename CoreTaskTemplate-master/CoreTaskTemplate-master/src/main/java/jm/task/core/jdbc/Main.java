01e6d5ff7631b39dd4e940af43668623056158e8
Ismaylov1 <71928727+Ismaylov1@users.noreply.github.com>
2020-11-19 21:28:59 +0500

    Add files via upload

---
+++ CoreTaskTemplate-master/CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,28 @@
+package jm.task.core.jdbc;
+
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        UserDaoJDBCImpl userService = new UserDaoJDBCImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Alex1", "sd", (byte) 18);
+        userService.saveUser("Alex2", "sd", (byte) 18);
+        userService.saveUser("Alex3", "sd", (byte) 18);
+        userService.saveUser("Alex4", "sd", (byte) 18);
+        userService.getAllUsers();
+        userService.removeUserById(2);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
+
+    }
+}
