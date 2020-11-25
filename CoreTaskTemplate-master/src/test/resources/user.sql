c1d47e5daf6dd4d64ecb9977be5bff1944b3366b
Ismaylov1 <71928727+Ismaylov1@users.noreply.github.com>
2020-11-25 15:16:13 +0500

    task114

--- CoreTaskTemplate-master/src/test/resources/user.sql
+++
@@ -1,5 +0,0 @@
-#insert into userdb1(name, age, secondName) values('Yablonan', 41, 'Yablonana.@mail.com');
-#select * from userdb1;
-#select name, age from userdb1 where name = 'Gondo';
-#update userdb1 set name = 'Gondo', age = 55 where id =1;
-#delete from userdb1 where id=7;
