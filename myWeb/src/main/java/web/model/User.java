b8d7a7f9a718e708afd50b7af49ab377875bb359
Ismaylov1 <71928727+Ismaylov1@users.noreply.github.com>
2020-12-02 19:10:08 +0500

    цу

---
+++ myWeb/src/main/java/web/model/User.java
@@ -0,0 +1,33 @@
+package web.model;
+
+import org.springframework.http.StreamingHttpOutputMessage;
+
+public class User {
+    String name;
+    String surname;
+    long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
